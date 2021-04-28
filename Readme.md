2c487c3fc168738ddfb91051475a49d6e4d04629
Aldo Medina <shdarkworld@gmail.com>
2021-04-28 19:04:29 +0000

    On, Off, Flash OK. Info added

---
+++ Readme.md
@@ -0,0 +1,7 @@
+Ejemplo LED3 On, Off y Flashing en Bash
+
+Utilice el argumento "on" para encender el LED3
+Utilice el argumento "off" para apagar el LED3
+Utilice el argumento "flash" para que el LED3 comience a Flashear (requiere sudo)
+
+Cualquier otro argumento no es válido
--- bashLED.sh
+++ bashLED.sh
@@ -4,27 +4,34 @@ LED_3=/sys/class/leds/beaglebone:green:usr3
 
 function removeTrigger
 {
+	# writes "none" in trigger
 	echo "none" >> "$LED_3/trigger"
 }
 
 echo "LED Script running"
 if [ $# != 1 ]; then 
+	# Checks if number of arguments are right 
 	echo "Number of arguments wrong"
 	exit 2
 fi
 if [ "$1" == "on" ]; then
+	# Turns on LED3
 	echo "LED3 on"
 	removeTrigger
 	echo "1" >> "$LED_3/brightness"
 elif [ "$1" == "off" ]; then
+	# Turns off LED3
 	echo "LED off"
 	removeTrigger
 	echo "0" >> "$LED_3/brightness"
 elif [ "$1" == "flash" ]; then
+	# LED3 starts flashing 50ms on, 50ms off (needs sudo)
 	echo "LED flashing"
 	removeTrigger
 	echo "timer" >> "$LED_3/trigger"
 	echo "50" >> "$LED_3/delay_on"
 	echo "50" >> "$LED_3/delay_off"
+else
+	echo "Wrong arguments"
 fi
 echo "Script Done"
